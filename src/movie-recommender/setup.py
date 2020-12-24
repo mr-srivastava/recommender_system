3a9df713e842e178ed24cc8c628e28ee6039edd0
Aadarsh Srivastava <58096256+mr-srivastava@users.noreply.github.com>
2020-12-24 17:11:08 +0530

    Structutred Project

---
+++ src/movie-recommender/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project deals with a Movie Recommendation System which recommend movies on the basis with similarities with the searched movie.',
+    author='Aadarsh Srivastava',
+    license='MIT',
+)
